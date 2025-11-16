4e861e46ce1432f3cf667c8e1d8d2d201ae6e64c
serhiisydorenko-wdl <sidorchik91@gmail.com>
2025-11-16 17:44:13 +0100

    ALL tasks are done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,63 @@
+class Key {
+  private signature: number;
+
+  constructor(){
+    this.signature = Math.random()
+  }
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key){
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key){
+    this.key = key;
+  }
+
+  comeIn(person: Person){
+   if (this.door) {
+      this.tenants.push(person);
+      console.log("Tenants + 1");
+    } else {
+      console.log("Door is closed");
+    }
+  }
+
+  abstract openDoor(key: Key): void
+}
+
+class MyHouse extends House {
+  constructor(key: Key){
+    super(key)
+  }
+
+  openDoor(key: Key){
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is open');
+    } else {
+      console.log("Wrong key!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
